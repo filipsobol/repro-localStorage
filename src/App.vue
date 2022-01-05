0b292af6f326dde10e0f4bb22a4c5e6fdaf29726
Filip Sobol <sobol.filip@gmail.com>
2022-01-05 23:11:30 +0100

    Add repro

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>State: {{ state }}</p>
+    
+    <div>
+      <button @click="setState">Set state</button>
+    </div>
+
+    <div>
+      <button @click="clearLocalStorage">Clear local storage</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useStorage } from "@vueuse/core";
+
+export default {
+  setup() {
+    const state = useStorage("state", null, localStorage);
+
+    function setState() {
+      state.value = "IS SET";
+    }
+
+    function clearLocalStorage() {
+      localStorage.clear();
+    }
+
+    return {
+      state,
+      setState,
+      clearLocalStorage
+    };
+  }
+}
+</script>
